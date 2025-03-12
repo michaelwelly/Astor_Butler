886be8e078205b7529219287f4c712a65e742da7
Michael_Poedinenko <michael.poedinenko.mxr@gmail.com>
2025-03-12 10:14:45 +0500

    clear page for art

---
+++ src/main/java/museon_online/astor_butler/AstorButlerApplication.java
@@ -0,0 +1,13 @@
+package museon_online.astor_butler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstorButlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstorButlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/museon_online/astor_butler/AstorButlerApplicationTests.java
@@ -0,0 +1,13 @@
+package museon_online.astor_butler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstorButlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
